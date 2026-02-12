70e000635919968551d2dd59b034dee58b8bfae1
donatto22 <ottanod22@gmail.com>
2026-02-11 21:58:17 -0500

    usando javascript

---
+++ clase9/script.js
@@ -0,0 +1,23 @@
+// // esto activa una alerta
+// // alert("Este es un mensaje")
+
+// // esto activa una alerta que recibe un mensaje
+// let numeroA = prompt('Coloca el numero inicial')
+// let numeroB = prompt('Coloca el numero secundario')
+// // console.log("Suma", Number(numeroA) + Number(numeroB))
+// // console.log(Number(numeroA) - Number(numeroB))
+// // console.log(Number(numeroA) * Number(numeroB))
+// // console.log(Number(numeroA) / Number(numeroB)) 
+
+// // concatenar forma 1
+// console.log("hola como " + "estas")
+// console.log("hola como " + numeroA)
+// // forma 2
+// console.log(`hola como ${numeroA}`)
+
+// const mayorEdad = prompt('Coloca tu edad')
+// console.log(mayorEdad >= 18 ? "Eres mayor de edad" : "Eres menor de edad")
+
+const edad = 18
+edad = 10
+console.log(edad)
